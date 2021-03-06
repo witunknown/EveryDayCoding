2ec77a52ea2b6e61465a11bc3b0fb70e8fa3e055
sunwenchi <562344464@qq.com>
2021-03-07 01:21:46 +0800

    算法分类:动态规划； 题目：跳台阶问题

--- src/main/java/dp/JumpFloor.java
+++ src/main/java/dp/JumpFloor.java
@@ -29,4 +29,28 @@ public class JumpFloor {
         //在第n阶台阶时，只能从第n-1或者第n-2阶台阶跳过来所以 f(n)=f(n-1)+f(n-2)
         return jumpFloor(target - 1) + jumpFloor(target - 2);
     }
+
+
+    //每次最多能跳n个台阶
+    public int jumpFloorN(int target, int n) {
+
+        if (target < 1) {
+            return 0;
+        }
+
+        if (target == 1) {
+            return 1;
+        }
+
+        if (target == 2) {
+            return 2;
+        }
+
+        int sum = 0;
+        //n大于target时，最多只能跳target
+        for (int i = 1; i < n && i < target; i++) {
+            sum += jumpFloorN(i, n);
+        }
+        return sum;
+    }
 }
