16dd93f12c9166908441a88f783a125d0617ace9
sunwenchi <562344464@qq.com>
2021-03-13 01:02:10 +0800

    规则校验：stack的使用

---
+++ src/main/java/other/ValidDemo.java
@@ -0,0 +1,35 @@
+package other;
+
+import java.util.Stack;
+
+/**
+ * Created byX on 2021-03-13 00:17
+ * Desc:
+ */
+public class ValidDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char temp = s.charAt(i);
+            if (temp == '(') {
+                stack.push(')');
+            } else if (temp == '[') {
+                stack.push(']');
+            } else if (temp == '{') {
+                stack.push('}');
+            } else if (stack.isEmpty() || stack.pop() != temp) {
+                return false;
+            }
+
+        }
+        if (!stack.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+}
