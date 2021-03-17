7d7ab57f3b2a0fef023bfaa4b9e55ce4ec674406
sunwenchi <562344464@qq.com>
2021-03-17 23:24:33 +0800

    股票交易，不限次数

---
+++ src/main/java/greedy/StockDemo.java
@@ -0,0 +1,23 @@
+package greedy;
+
+/**
+ * Created byX on 2021-03-17 23:15
+ * Desc:股票交易 不限次数
+ */
+public class StockDemo {
+    public static void main(String[] args) {
+
+    }
+
+    public int maxProfit(int[] prices) {
+        int total = 0;
+        if (prices.length <= 1) {
+            return total;
+        }
+        for (int i = 0; i < prices.length - 1; i++) {
+            int temp = prices[i + 1] - prices[i];
+            total += temp > 0 ? temp : 0;
+        }
+        return total;
+    }
+}
